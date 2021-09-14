de93da33ffca763fdf9cf02357f9d47e81f29666
Johan <joha034p@stud.kea.dk>
2021-09-14 11:01:06 +0200

    sort toggles direction

--- animalbase.js
+++ animalbase.js
@@ -84,24 +84,35 @@ function isDog(animal) {
 
 function selectSort(event) {
   const sortBy = event.target.dataset.sort;
-  console.log(`User selected ${sortBy}`);
-  sortList(sortBy);
+  const sortDir = event.target.dataset.sortDirection;
+
+  // Toggle the direction!
+  if (sortDir === "asc") {
+    event.target.dataset.sortDirection = "desc";
+  } else {
+    event.target.dataset.sortDirection = "asc";
+  }
+  console.log(`User selected ${sortBy} - ${sortDir}`);
+  sortList(sortBy, sortDir);
 }
 
-function sortList(sortBy) {
+function sortList(sortBy, sortDir) {
   let sortedList = allAnimals;
+  let direction = 1;
+  if (sortDir === "desc") {
+    direction = -1;
+  } else {
+    direction = 1;
+  }
 
-  // if (sortBy === "name") {
   sortedList = sortedList.sort(sortByProperty);
-  // } else if (sortBy === "type") {
-  //   sortedList = sortedList.sort(sortByType);
-  // }
+
   function sortByProperty(animalA, animalB) {
     console.log(`sortBy is ${sortBy}`);
     if (animalA[sortBy] < animalB[sortBy]) {
-      return -1;
+      return -1 * direction;
     } else {
-      return 1;
+      return 1 * direction;
     }
   }
   displayList(sortedList);
